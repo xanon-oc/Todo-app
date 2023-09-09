9de45f67ced57b89779fee6b29a19fbf9a5bfb91
Niloy Roy <niloyroy184@gmail.com>
2023-09-09 11:25:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      {/* <!-- navbar --> */}
+      <Navbar />
+
+      <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+        {/* <!-- header --> */}
+        <Header />
+        <hr className="mt-4" />
+
+        {/* <!-- todo list --> */}
+        <TodoList />
+
+        <hr className="mt-4" />
+
+        {/* <!-- footer --> */}
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import notes from "../assets/images/notes.png";
+import double from "../assets/images/double-tick.png";
+const Header = () => {
+  return (
+    <div>
+      <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+        <img src={notes} className="w-6 h-6" alt="Add todo" />
+        <input
+          type="text"
+          placeholder="Type your todo"
+          className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+        />
+        <button type="submit" className="appearance-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        </button>
+      </form>
+
+      <ul className="flex justify-between my-4 text-xs text-gray-500">
+        <li className="flex space-x-1 cursor-pointer">
+          <img className="w-4 h-4" src={double} alt="Complete" />
+          <span>Complete All Tasks</span>
+        </li>
+        <li className="cursor-pointer">Clear completed</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
